5d33d7752e50f997ff6b403f6631b88aacdfc100
Zaidab <zaida.burrola@gmail.com>
2014-11-29 09:42:38 -0800

    dao hashtag

--- src/dream/dao/HashtagDAO.java
+++ src/dream/dao/HashtagDAO.java
@@ -1,5 +1,16 @@
 package dream.dao;
 
+import Entidad.Hashtag;
+import Entidad.Idea;
+
 public class HashtagDAO {
+	Hashtag hashtag;
+	String nombre;
+	Idea idea;
+	
+	public HashtagDAO(){
+		super();
+	}
+	
 
 }
